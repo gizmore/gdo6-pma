93844fe1f0da3f9af887ee6d8cdec13d8001dd7b
gizmore <gizmore@wechall.net>
2020-02-07 11:37:38 +0100

    6.10-r5013_bootstrap_vote

--- Method/Bridge.php
+++ Method/Bridge.php
@@ -2,6 +2,8 @@
 namespace GDO\PhpMyAdmin\Method;
 
 use GDO\Core\Method;
+use GDO\Core\Website;
+use GDO\PhpMyAdmin\Module_PhpMyAdmin;
 
 final class Bridge extends Method
 {
@@ -9,5 +11,7 @@ final class Bridge extends Method
 	
 	public function execute()
 	{
+		$url = Module_PhpMyAdmin::instance()->href_pma();
+		return Website::redirect($url);
 	}
 }
