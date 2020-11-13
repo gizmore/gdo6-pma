11e078e4fb36796a03a838dca58a5f6acc13d854
gizmore <gizmore@wechall.net>
2020-11-13 07:45:21 +0100

    6.10-r9375_onInitSidebar

--- Module_PhpMyAdmin.php
+++ Module_PhpMyAdmin.php
@@ -4,18 +4,20 @@ namespace GDO\PhpMyAdmin;
 use GDO\Core\GDO_Module;
 use GDO\DB\GDT_Checkbox;
 use GDO\User\GDO_User;
-use GDO\UI\GDT_Bar;
 use GDO\UI\GDT_Link;
+use GDO\UI\GDT_Page;
 
 /**
  * Wrap PhpMyAdmin installation.
  * 
  * @author gizmore
- * @since 6.10
  * @version 6.10
+ * @since 6.10
  */
 final class Module_PhpMyAdmin extends GDO_Module
 {
+    public $module_priority = 80;
+    
 	public function href_pma() { return GWF_WEB_ROOT . 'GDO/PhpMyAdmin/phpmyadmin'; }
 
 	##############
@@ -29,19 +31,20 @@ final class Module_PhpMyAdmin extends GDO_Module
 	##############
 	public function getConfig()
 	{
-		return array(
+		return [
 			GDT_Checkbox::make('pma_sidebar')->initial('0'),
-		);
+		];
 	}
 	public function cfgSidebar() { return $this->getConfigValue('pma_sidebar'); }
 
 	#############
 	### Hooks ###
 	#############
-	public function hookRightBar(GDT_Bar $bar)
+	public function onInitSidebar()
 	{
 		if ($this->cfgSidebar() && GDO_User::current()->isAdmin())
 		{
+		    $bar = GDT_Page::$INSTANCE->rightNav;
 			$bar->addField(GDT_Link::make('link_pma')->href($this->href('Bridge')));
 		}
 	}
