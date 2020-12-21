bccad23802d1d20cac5e0f45a9e18a31ed1d7cea
gizmore <gizmore@wechall.net>
2020-12-21 18:17:34 +0100

    6.10-r9405_unit_testing

--- Module_PhpMyAdmin.php
+++ Module_PhpMyAdmin.php
@@ -20,6 +20,8 @@ final class Module_PhpMyAdmin extends GDO_Module
     
 	public function href_pma() { return GWF_WEB_ROOT . 'GDO/PhpMyAdmin/phpmyadmin'; }
 
+	public function thirdPartyFolders() { return ['/phpmyadmin/']; }
+	
 	##############
 	### Module ###
 	##############
