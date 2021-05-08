16433af8d58d5cf47f39daee22d1b2390c8900ce
gizmore <gizmore@wechall.net>
2021-05-08 04:58:26 +0200

    6.10.2-r1082_tbs_migrate

--- Method/Bridge.php
+++ Method/Bridge.php
@@ -20,7 +20,7 @@ final class Bridge extends Method
 	public function execute()
 	{
 		$url = Module_PhpMyAdmin::instance()->href_pma();
-		$par = GDT_Panel::make()->html(t('pma_username_password', [GWF_DB_USER, GWF_DB_PASS]));
+		$par = GDT_Panel::make()->html(t('pma_username_password', [GDO_DB_USER, GDO_DB_PASS]));
 		$btn = GDT_Link::make('btn_launch_pma')->targetBlank()->href($url);
 		return GDT_Response::makeWith($par, GDT_Bar::makeWith($btn)->horizontal());
 	}
